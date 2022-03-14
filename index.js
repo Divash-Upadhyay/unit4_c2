0fb3d3082f5a822eeebbd3709bbdbe310bedafd4
Divash-Upadhyay <76831671+Divash-Upadhyay@users.noreply.github.com>
2022-03-14 13:00:02 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,147 @@
+const express = require("express");
+const { type } = require("express/lib/response");
+
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+const connect = () => {
+    return mongoose.connect(
+        ("mongodb+srv://Divash:qwerty67890@cluster0.6lshx.mongodb.net/webb14?retryWrites=true&w=majority")
+    );
+};
+
+const userSchemas = new mongoose.Schema(
+    {
+        id: { type: Number, required: true },
+        firstName: { type: String, required: true },
+        lastName: { type: String, required: true },
+        middleName: { type: String, required: true },
+        age: { type: Number, required: true },
+        email: { type: String, required: true },
+        Adress: { type: String, required: true },
+        gender: { type: String, required: true, default: "female" },
+        type: { type: String, required: false },
+    },
+    {
+        versionKey: false,
+        timestamps: true,
+    }
+
+);
+
+
+const User = mongoose.model("user", userSchemas);
+
+
+const branchdetailsSchemas = new mongoose.Schema(
+    {
+        id: { type: Number, required: true },
+        Name: { type: String, required: true },
+        Adress: { type: String, required: true },
+        IFSC: { type: String, required: true },
+        MICR: { type: Number, required: true },
+
+        user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+            required: true,
+        },
+    },
+    {
+        versionKey: false,
+        timestamps: true,
+    }
+);
+
+const branchDetail = mongoose.model("branchDetail", branchdetailsSchemas);
+
+
+const masterAccountSchemas = new mongoose.Schema(
+    {
+        id: { type: Number, required: true },
+        balance: { type: Number, required: true },
+        user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+            required: true,
+        },
+    },
+    {
+        versionKey: false,
+        timestamps: true,
+    }
+
+);
+
+
+const masterAccount = mongoose.model("masterAccount", masterAccountSchemas);
+
+const savingsAccountSchemas = new mongoose.Schema(
+    {
+        id:{type:Number,required:true},
+        account_number:{type:Number,required:true, unique:true},
+        balance:{type:Number,required:true},
+        interestRate:{type:Number,required:true},
+
+        user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+            required: true,
+        },
+        master_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "masterAccount",
+            required: true,
+        },
+        
+    },
+    {
+        versionKey: false,
+        timestamps: true,
+    }
+);
+
+
+const savingsAccount = mongoose.model("savingsAccount", savingsAccountSchemas);
+
+const fixedAccountSchemas = new mongoose.Schema(
+    {
+        id:{type:Number,required:true},
+        account_number:{type:Number,required:true,unique:true},
+        balance:{type:Number,required:true},
+        interestRate:{type:Number,required:true},
+        startDate:{type:Number,required:true},
+        maturityDate:{type:Number,required:true},
+
+        user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+            required: true,
+        },
+        master_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "masterAccount",
+            required: true,
+        },
+    },
+    {
+        versionKey: false,
+        timestamps: true,
+    }
+)
+
+
+const fixedAccount = mongoose.model("fixedAccount", fixedAccountSchemas);
+
+
+app.listen(2347, async function () {
+    try {
+        await connect();
+        console.log("runnimg on port 2347");
+    } catch (e) {
+        console.log(e.message);
+    }
+});
